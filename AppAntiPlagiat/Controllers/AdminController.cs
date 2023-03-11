587bed22dea6c0acd08b0a6e9bef8aa016d1063b
ikram assabbar <imalak419@gmail.com>
2023-03-11 13:56:20 +0100

    espace etudiant

--- AppAntiPlagiat/Controllers/AdminController.cs
+++ AppAntiPlagiat/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using AppAntiPlagiat.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
 
@@ -15,12 +16,12 @@ namespace AppAntiPlagiat.Controllers
         private readonly UserManager<Utilisateur> userManager;
         private readonly ApplicationDbContext applicationDbContext;
 
-        public AdminController(UserManager<Utilisateur> userManager,ApplicationDbContext applicationDbContext)
+        public AdminController(UserManager<Utilisateur> userManager, ApplicationDbContext applicationDbContext)
         {
             this.userManager = userManager;
             this.applicationDbContext = applicationDbContext;
         }
-        
+
         public IActionResult Dashboard()
         {
             ViewBag.Ltype = "admin";
@@ -59,7 +60,7 @@ namespace AppAntiPlagiat.Controllers
                     ViewBag.ajoutNV = "ajouté";
                     return View(model);
                 }
-                foreach(var error in result.Errors)
+                foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError("", error.Description);
                 }
@@ -95,8 +96,8 @@ namespace AppAntiPlagiat.Controllers
                     NormalizedEmail = model.Email.ToUpper(),
                     Niveau = model.Niveau,
                     IMGurl = "~/images/Users/NewUser.png",
-                    Filiere= model.Filiere,
-                    CNE = model.CNE 
+                    Filiere = model.Filiere,
+                    CNE = model.CNE
                 };
                 //add user
                 var result = await userManager.CreateAsync(nvEtudiant, model.Password);
@@ -122,11 +123,197 @@ namespace AppAntiPlagiat.Controllers
             var etudiants = await userManager.GetUsersInRoleAsync("etudiant");
             return View(etudiants);
         }
-        public IActionResult Affectation()
+
+        [HttpPost]
+        public ActionResult SupprimerAffectation(int id)
+        {
+            Encadre encadrement = applicationDbContext.Encadre.Find(id);
+            if (encadrement != null)
+            {
+                applicationDbContext.Encadre.Remove(encadrement);
+                applicationDbContext.SaveChanges();
+                TempData["SuccessMessage"] = "L'étudiant a été supprimé avec succès.";
+            }
+
+            return RedirectToAction("ListeAffectation");
+        }
+        [HttpGet]
+        public ActionResult ListeAffectation(int id)
         {
             ViewBag.Ltype = "admin";
+
+            var encadreList = from Encadre in applicationDbContext.Encadre
+                              join etudiant in applicationDbContext.Utilisateurs on Encadre.EtudiantId equals etudiant.Id
+                              join enseignant in applicationDbContext.Utilisateurs on Encadre.EnseignantId equals enseignant.Id
+                              select new
+                              {
+                                  Id = Encadre.Id,
+                                  EtudiantNom = etudiant.Nom,
+                                  EtudiantPrenom = etudiant.Prenom,
+                                  EnseignantNom = enseignant.Nom,
+                                  EnseignantPrenom = enseignant.Prenom,
+                                  Encadre.TypeStage,
+                                  EtudiantFiliere = etudiant.Filiere,
+                              };
+
+            return View(encadreList);
+        }
+        [HttpPost]
+        public ActionResult AffecterEncadrement(string EtudiantId, string EnseignantId, string TypeStage)
+        {
+            if (ModelState.IsValid)
+            {
+                // Créer un nouvel objet Encadre avec les valeurs des champs
+                Encadre newEncadre = new Encadre
+                {
+                    EtudiantId = EtudiantId,
+                    EnseignantId = EnseignantId,
+                    TypeStage = TypeStage
+                };
+                if (newEncadre != null)
+                {   // Ajouter l'objet Encadre à la base de données
+                    applicationDbContext.Encadre.Add(newEncadre);
+                    applicationDbContext.SaveChanges();
+                    TempData["SuccesMessage"] = "Affectation réussie.";
+                }
+
+                else
+                {
+                    TempData["ErrorMessage"] = "Vous devez sélectionner un étudiant et un enseignant pour effectuer une affectation.";
+                }
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Vous devez sélectionner un étudiant et un enseignant pour effectuer une affectation.";
+            }
+
+
+            return RedirectToAction("Affectation");
+        }
+        public async Task<IActionResult> Affectation(string EtudiantId, string EnseignantId, string TypeStage)
+        {
+
+
+            ViewBag.Ltype = "admin";
+
+            var etudiants = await userManager.GetUsersInRoleAsync("etudiant");
+
+            var etudiantList = new SelectList(etudiants, "Id", "NomComplet");
+            ViewBag.EtudiantList = etudiantList;
+            ViewBag.Etudiants = new SelectList(etudiants, "Id", "NomComplet");
+
+            //recup enseig
+            var enseignants = await userManager.GetUsersInRoleAsync("enseignant");
+            var enseignantList = new SelectList(enseignants, "Id", "NomComplet");
+            ViewBag.EnseignantList = enseignantList;
+            ViewBag.Enseignants = new SelectList(enseignants, "Id", "NomComplet");
+
+
             return View();
         }
+        public async Task<IActionResult> AffectationAlleatoire(string TypeStage, String Niveau, string Filiere)
+        {
+
+            ViewBag.Ltype = "admin";
+            // Récupération des données nécessaires depuis la base de données
+            var etudiants = await userManager.GetUsersInRoleAsync("etudiant");
+            etudiants = etudiants.Where(e => e.Niveau == Niveau).ToList();
+            etudiants = etudiants.Where(e => e.Filiere == Filiere).ToList();
+            //enseign
+            var enseignants = await userManager.GetUsersInRoleAsync("enseignant");
+            // enseignants = enseignants.Where(e => e.Niveau == Niveau).ToList();
+
+            var affectations = new List<Encadre>();
+            // Affectation aléatoire des étudiants aux enseignants
+            Random random = new Random();
+            foreach (var etudiant in etudiants)
+            {
+                int index = random.Next(0, enseignants.Count);
+                var enseignant = enseignants[index];
+                if (ModelState.IsValid)
+                {
+                    if (!string.IsNullOrEmpty(TypeStage))
+                    {
+                        // Enregistrement de l'affectation dans la base de données
+
+                        applicationDbContext.Encadre.Add(new Encadre
+                        {
+                            EtudiantId = etudiant.Id,
+                            EnseignantId = enseignant.Id,
+                            TypeStage = TypeStage // Récupération du type de stage spécifié par l'utilisateur
+
+                        });
+                        TempData["SuccesMessage"] = "Affectation réussie.";
+                        applicationDbContext.SaveChanges();
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Vous devez sélectionner un étudiant et un enseignant pour effectuer une affectation.";
+                    }
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Vous devez sélectionner un étudiant et un enseignant pour effectuer une affectation.";
+
+                }
+                // Retrait de l'enseignant de la liste pour éviter les doublons
+                //enseignants.RemoveAt(index);
+            }
+
+            return View("AffectationAlleatoire"); // Redirection vers la page d'affichage des affectations
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Modifier(int id)// modifier une affectation
+        {
+            var affectation = await applicationDbContext.Encadre.FindAsync(id);
+            if (affectation == null)
+            {
+                return NotFound();
+            }
+
+            var etudiants = await userManager.GetUsersInRoleAsync("etudiant");
+            var etudiantList = new SelectList(etudiants, "Id", "NomComplet", affectation.EtudiantId);
+
+            var enseignants = await userManager.GetUsersInRoleAsync("enseignant");
+            var enseignantList = new SelectList(enseignants, "Id", "NomComplet", affectation.EnseignantId);
+
+
+            ViewBag.EtudiantList = etudiantList;
+            ViewBag.EnseignantList = enseignantList;
+            applicationDbContext.Encadre.Remove(affectation);
+            applicationDbContext.SaveChanges();
+            ViewBag.Ltype = "admin";
+
+            return View("Affectation");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Modifier(Encadre encadre)
+        {
+            if (ModelState.IsValid)
+            {
+                applicationDbContext.Update(encadre);
+                await applicationDbContext.SaveChangesAsync();
+
+                return RedirectToAction("ListeAffectations");
+            }
+
+            var etudiants = await userManager.GetUsersInRoleAsync("etudiant");
+            var etudiantList = new SelectList(etudiants, "Id", "NomComplet", encadre.EtudiantId);
+
+            var enseignants = await userManager.GetUsersInRoleAsync("enseignant");
+            var enseignantList = new SelectList(enseignants, "Id", "NomComplet", encadre.EnseignantId);
+
+
+            ViewBag.EtudiantList = etudiantList;
+            ViewBag.EnseignantList = enseignantList;
+            ViewBag.Ltype = "admin";
+
+            return View(encadre);
+        }
+      
         public IActionResult Messages(int Page = 1)
         {
             ViewBag.Ltype = "admin";
@@ -147,7 +334,7 @@ namespace AppAntiPlagiat.Controllers
                 msg.tempsPasse = TimeAgo(message.dateEnvoie);
                 liste.Add(msg);
             }
-            
+
             int TotalMessageCount = applicationDbContext.Messages.Count();
             ViewBag.TotalMessageCount = TotalMessageCount;
 
@@ -201,7 +388,8 @@ namespace AppAntiPlagiat.Controllers
         public IActionResult DeleteSelectedEnseignants(string[] enseignantIds)
         {
             // Delete the selected messages from the database
-            if (enseignantIds.Length != 0) {
+            if (enseignantIds.Length != 0)
+            {
                 foreach (var Id in enseignantIds)
                 {
                     var enseignant = applicationDbContext.Utilisateurs.Find(Id);
@@ -217,11 +405,12 @@ namespace AppAntiPlagiat.Controllers
         public IActionResult DeleteEnseignant(string enseigId)
         {
             var enseignant = applicationDbContext.Utilisateurs.Find(enseigId);
-            if(enseignant != null) {
+            if (enseignant != null)
+            {
                 applicationDbContext.Utilisateurs.Remove(enseignant);
                 applicationDbContext.SaveChanges();
             }
-           
+
             return RedirectToAction("RechSuppModEnseignant");
         }
         [HttpPost]
@@ -238,7 +427,7 @@ namespace AppAntiPlagiat.Controllers
         }
 
         [HttpGet]
-        public async Task<JsonResult> FiltrerEtudiant(string filiere , string nv)
+        public async Task<JsonResult> FiltrerEtudiant(string filiere, string nv)
         {
             if (filiere == "tout" && nv == "tout")
             {
@@ -255,7 +444,8 @@ namespace AppAntiPlagiat.Controllers
                 var users = applicationDbContext.Utilisateurs.Where(x => x.Filiere == filiere).ToList();
                 return Json(users);
             }
-            else {
+            else
+            {
                 var users = applicationDbContext.Utilisateurs.Where(x => x.Filiere == filiere && x.Niveau == nv).ToList();
                 return Json(users);
             }
@@ -265,13 +455,13 @@ namespace AppAntiPlagiat.Controllers
         {
             ViewBag.Ltype = "admin";
             var enseignant = applicationDbContext.Utilisateurs.Find(enseigId);
-            if(enseignant != null)
+            if (enseignant != null)
             {
                 ModifierEnseignantViewModel model = new ModifierEnseignantViewModel()
                 {
                     Departement = enseignant.Departement,
                     Email = enseignant.Email,
-                    Id= enseignant.Id,
+                    Id = enseignant.Id,
                     Nom = enseignant.Nom,
                     Prenom = enseignant.Prenom
                 };
@@ -283,17 +473,18 @@ namespace AppAntiPlagiat.Controllers
         public IActionResult ModifyEnseignant(ModifierEnseignantViewModel model)
         {
             var enseignant = applicationDbContext.Utilisateurs.Find(model.Id);
-            if(enseignant != null) {
+            if (enseignant != null)
+            {
                 enseignant.Email = model.Email;
                 enseignant.UserName = model.Email;
                 enseignant.NormalizedEmail = model.Email.ToUpper();
                 enseignant.Departement = model.Departement;
-                enseignant.Prenom= model.Prenom;
+                enseignant.Prenom = model.Prenom;
                 enseignant.Nom = model.Nom;
                 applicationDbContext.SaveChanges();
             }
-            
-            
+
+
             return RedirectToAction("RechSuppModEnseignant");
         }
         public IActionResult ModifyEtud(string etudId)
@@ -339,10 +530,10 @@ namespace AppAntiPlagiat.Controllers
         public IActionResult DeleteMessage(int Id)
         {
             // Delete the selected messages from the database
-            
-                var message = applicationDbContext.Messages.Find(Id);
-                applicationDbContext.Messages.Remove(message);
-            
+
+            var message = applicationDbContext.Messages.Find(Id);
+            applicationDbContext.Messages.Remove(message);
+
             applicationDbContext.SaveChanges();
 
             // Redirect to the inbox page
@@ -352,20 +543,21 @@ namespace AppAntiPlagiat.Controllers
         {
             ViewBag.Ltype = "admin";
             List<Message> msg = applicationDbContext.Messages.ToList();
-            if (msg != null && msg.Count!=0)
+            if (msg != null && msg.Count != 0)
             {
-                
+
                 Message m = msg.Find(x => x.Id == currentMsgId);
-                if (m != null) { 
+                if (m != null)
+                {
                     if (msg.IndexOf(m) != msg.Count - 1 && msg.IndexOf(m) != 0)
                     {
                         int prev = msg[msg.IndexOf(m) - 1].Id;
                         ViewBag.prev = prev;
                         int suiv = msg[msg.IndexOf(m) + 1].Id;
                         ViewBag.suiv = suiv;
-                   
+
                     }
-                    else if(msg.IndexOf(m) == 0)
+                    else if (msg.IndexOf(m) == 0)
                     {
                         int suiv = msg[msg.IndexOf(m) + 1].Id;
                         ViewBag.suiv = suiv;
@@ -444,7 +636,8 @@ namespace AppAntiPlagiat.Controllers
         [HttpGet]
         public async Task<JsonResult> FindByDepartement(string op)
         {
-            if (op != "tout") {
+            if (op != "tout")
+            {
                 var users = applicationDbContext.Utilisateurs.Where(x => x.Departement == op).ToList();
                 return Json(users);
             }
--- AppAntiPlagiat/Controllers/EtudiantController.cs
+++ AppAntiPlagiat/Controllers/EtudiantController.cs
@@ -1,29 +1,125 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AppAntiPlagiat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AppAntiPlagiat.Controllers
 {
     [Authorize(Roles = "etudiant")]
     public class EtudiantController : Controller
     {
+        private readonly IWebHostEnvironment _env;
+        private readonly UserManager<Utilisateur> userManager;
+        private readonly ApplicationDbContext applicationDbContext;
+        public EtudiantController(UserManager<Utilisateur> userManager, ApplicationDbContext applicationDbContext, IWebHostEnvironment env)
+        {
+            this.userManager = userManager;
+            this.applicationDbContext = applicationDbContext;
+            _env = env;
+        }
         public IActionResult Profile()
         {
             ViewBag.Ltype = "etudiant";
             return View();
         }
-        public IActionResult ImporterRapport()
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeposerRapp(Rapport model,String TypeS)
+        {
+            /*if (ModelState.IsValid)
+            {*/
+            if (model.File != null && model.File.Length > 0)
+            {
+                var nomFichier = Path.GetFileName(model.File.FileName.Trim('"'));
+                var cheminFichier = Path.Combine(_env.WebRootPath, "rapports", nomFichier);
+
+                // Vérifier si le dossier de destination existe et le créer si nécessaire
+                var dossierDestination = Path.GetDirectoryName(cheminFichier);
+                if (!Directory.Exists(dossierDestination))
+                {
+                    Directory.CreateDirectory(dossierDestination);
+                }
+
+                using (var stream = new FileStream(cheminFichier, FileMode.Create))
+                {
+                    await model.File.CopyToAsync(stream);
+                }
+
+                // Enregistrer le rapport dans la base de données
+                var rapport = new Rapport
+                {
+                    EtudiantId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                    Intitulé = nomFichier,
+                    Chemin = cheminFichier,
+                    DateDepot = DateTime.Now,
+                    Type = TypeS,
+                    Validé = false
+
+                };
+               
+                applicationDbContext.Rapports.Add(rapport);
+                await applicationDbContext.SaveChangesAsync();
+       
+
+                return RedirectToAction("VosRapports");
+                //  }
+            }
+
+            ViewBag.Ltype = "etudiant";
+            return RedirectToAction("VosRapports");
+
+
+        }
+        public async Task<IActionResult> ImporterRapport()
         {
             ViewBag.Ltype = "etudiant";
             return View();
         }
+
         public IActionResult VosRapports()
         {
             ViewBag.Ltype = "etudiant";
-            return View();
+            if (User.Identity.IsAuthenticated)
+                {
+                    string etudiantId = userManager.GetUserId(User);
+                List<Rapport> mesRapports = applicationDbContext.Rapports.Where(r => r.EtudiantId == etudiantId).ToList();
+                return View(mesRapports);
+            }
+            return View(); 
+            
+            // Récupérer les rapports de cet étudiant à partir de la base de données
+            
+
+            // Passer les rapports récupérés à la vue
+            
+          
+            
         }
-        public IActionResult ListeEnseignants()
+        public async Task<IActionResult> ListeEnseignants()
         {
             ViewBag.Ltype = "etudiant";
+            if (User.Identity.IsAuthenticated)
+            {
+                string etudiantId = userManager.GetUserId(User);
+
+                // Récupérer les enseignants affectés à l'étudiant connecté
+                var enseignantsAffectes = applicationDbContext.Encadre
+                    .Where(e => e.EtudiantId == etudiantId)
+                    .Select(e => e.Enseignant)
+                    .ToList();
+
+                // Récupérer les types de stage affectés à l'étudiant connecté
+                var typesStageAffectes = applicationDbContext.Encadre
+                    .Where(e => e.EtudiantId == etudiantId)
+                    .Select(e => e.TypeStage)
+                    .ToList();
+                var model = new Tuple<List<Utilisateur>, List<string>>(enseignantsAffectes, typesStageAffectes);
+                return View(model);
+            }
             return View();
         }
 
